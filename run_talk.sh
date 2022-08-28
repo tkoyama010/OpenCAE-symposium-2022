04694f0338ec1399bacd4bbeb3efa626c7ef7169
Tetsuo Koyama <tkoyama010@gmail.com>
2022-08-28 23:21:03 +0900

    Fix by check talk

--- run_talk.sh
+++ run_talk.sh
@@ -1,11 +1,7 @@
-# tmpfile1=$(mktemp --suffix=.wav)
-# tmpfile2=$(mktemp --suffix=.wav)
+tmpfile1=$(mktemp --suffix=.wav)
+tmpfile2=$(mktemp --suffix=.wav)
 rm -f talk.wav
-rm -f out.wav
-rm -f out2.wav
 cat index.md | \
-perl -pe "s/^\n//g" | \
-perl -pe "s/この節/このせつ/g" | \
 perl -pe "s/Binder/バインダー/g" | \
 perl -pe "s/CalculiX/カリキュリエックス/g" | \
 perl -pe "s/Coarse model/コースモデル/g" | \
@@ -20,6 +16,7 @@ perl -pe "s/FrontISTR/フロントアイスター/g" | \
 perl -pe "s/GetFEM/ゲットエフイーエム/g" | \
 perl -pe "s/Jupyter Lab/ジュパイターラボ/g" | \
 perl -pe "s/Jupyter/ジュパイター/g" | \
+perl -pe "s/Linear elastic solid/リニアーエラスティクソリッド/g" | \
 perl -pe "s/Matlab/マトラボ/g" | \
 perl -pe "s/Matplotlib/マットプロットリブ/g" | \
 perl -pe "s/Newmark/ニューマーク/g" | \
@@ -29,6 +26,7 @@ perl -pe "s/ParaView/パラビュー/g" | \
 perl -pe "s/Pull Request/プルリクエスト/g" | \
 perl -pe "s/PyVista/パイビスタ/g" | \
 perl -pe "s/Python/パイソン/g" | \
+perl -pe "s/Read The Docs/リードザドックス/g" | \
 perl -pe "s/Salome-Meca/サロメメカ/g" | \
 perl -pe "s/Sandia/サンディア/g" | \
 perl -pe "s/SciPy/サイパイ/g" | \
@@ -41,26 +39,24 @@ perl -pe "s/^###//g" | \
 perl -pe "s/^##//g" | \
 perl -pe "s/^#//g" | \
 perl -pe "s/^- //g" | \
+perl -pe "s/^\n//g" | \
 perl -pe "s/issue/イシュー/g" | \
 perl -pe "s/meshio/メッシュアイオー/g" | \
 perl -pe "s/pyinstaller/パイインストーラー/g" | \
 perl -pe "s/pyvista.example/パイビスタのイグザンプル/g" | \
 perl -pe "s/trimesh/トリメッシュ/g" | \
+perl -pe "s/この節/このせつ/g" | \
 perl -pe "s/パイソンic/パイソニック/g" | \
 perl -pe "s/後者の値/後者のあたい/g" | \
 while read line
 do
     echo $line
-    echo $line | open_jtalk -x /var/lib/mecab/dic/open-jtalk/naist-jdic -m /usr/share/hts-voice/nitech-jp-atr503-m001/nitech_jp_atr503_m001.htsvoice -ow out.wav
-    aplay out.wav
+    echo $line | open_jtalk -x /var/lib/mecab/dic/open-jtalk/naist-jdic -m /usr/share/hts-voice/nitech-jp-atr503-m001/nitech_jp_atr503_m001.htsvoice -ow $tmpfile1
     if [ -e talk.wav ]; then
-        sox talk.wav out.wav out2.wav
-        aplay out2.wav
-        cp out2.wav talk.wav
-        aplay talk.wav
+        sox talk.wav $tmpfile1 $tmpfile2
+        cp $tmpfile2 talk.wav
     else
-        cp out.wav talk.wav
-        aplay talk.wav
+        cp $tmpfile1 talk.wav
     fi
-    aplay talk.wav
 done
+time aplay talk.wav
